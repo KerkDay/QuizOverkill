08a1d4d3e62eab2731b64a2ccaf51d75c808c6b4
KerkDay <kerkday@outlook.com>
2021-03-03 15:53:08 -0700

    Fixed some React Based SVG Errors, changed existing icons to FeatherIcons, added Save and Package icons, added Documentation

--- src/extra/Icons.jsx
+++ src/extra/Icons.jsx
@@ -15,7 +15,7 @@ import React from 'react';
  */
 const SVGBlock = (props) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="76" height="76" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       {props.children}
     </svg>
   )
@@ -78,6 +78,15 @@ const Icons = {
         <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
         <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
         <line x1="12" y1="22.08" x2="12" y2="12"></line>
+      </SVGBlock>
+    )
+  },
+  Burger: (props) => {
+    return(
+      <SVGBlock {...props}>
+        <line x1="3" y1="12" x2="21" y2="12"></line>
+        <line x1="3" y1="6" x2="21" y2="6"></line>
+        <line x1="3" y1="18" x2="21" y2="18"></line>
       </SVGBlock>
     )
   }
