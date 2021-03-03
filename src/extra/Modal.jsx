0a53a61ff159b6049164b374ee1239484db9236b
KerkDay <kerkday@outlook.com>
2021-03-03 15:57:33 -0700

    Modals should be FIXED not ABSOLUTE

--- src/extra/Modal.jsx
+++ src/extra/Modal.jsx
@@ -5,7 +5,7 @@ function Modal(props) {
     return null
   } else {
     return (
-      <div className="bg-black bg-opacity-50 absolute inset-0 flex align-middle justify-center" onClick={() => { props.closeModal() }}>
+      <div className="bg-black bg-opacity-50 fixed z-40 inset-0 flex align-middle justify-center" onClick={() => { props.closeModal() }}>
         <div className="bg-white p-4 rounded-lg">
           {props.children}
         </div>
