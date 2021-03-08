a6f5bae9cfffc54aa3da93f6c194f16a73ba7f9e
KerkDay <kerkday@outlook.com>
2021-03-07 17:19:31 -0700

    Removed the set size of this icon, so it can be bigger elsewhere.

--- src/extra/Icons.jsx
+++ src/extra/Icons.jsx
@@ -15,7 +15,7 @@ import React from 'react';
  */
 const SVGBlock = (props) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       {props.children}
     </svg>
   )
