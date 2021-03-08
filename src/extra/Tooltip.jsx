4bb5a54a9053d9239fc0a5ec0bf98ffa1e90e848
KerkDay <kerkday@outlook.com>
2021-03-07 17:20:46 -0700

    Still has it's issues. Changed className variable to template strings. Looks better, easier to handle.

--- src/extra/Tooltip.jsx
+++ src/extra/Tooltip.jsx
@@ -5,17 +5,17 @@ function Tooltip(props) {
   let side = "top-center"
   typeof props.side !== 'undefined' ? side = props.side : side = "top-center"
 
-  let c = "opacity-0 group-hover:opacity-100 text-xs min-w-max pointer-events-none bg-black text-white absolute transition-opacity duration-200 p-2 rounded-md " // Class Name
+  let c = "opacity-0 group-hover:opacity-100 text-xs min-w-max pointer-events-none bg-black text-white absolute transition-opacity duration-200 p-2 rounded-md z-50" // Class Name
 
   switch(side) {
     case "top-center":
-      c = c + "left-1/2 bottom-full transform -translate-x-1/2"
+      c = `${c} left-1/2 bottom-full transform -translate-x-1/2`
       break
     case "top-left":
-      c = c + "left-0 bottom-full"
+      c = `${c} left-0 bottom-full`
       break
     case "top-right":
-      c = c + "right-0 bottom-full"
+      c = `${c} right-0 bottom-full`
       break
   }
 
