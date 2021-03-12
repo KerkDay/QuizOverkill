c35bea908a7bdf9d7e786fcd25d185bb7c853949
KerkDay <kerkday@outlook.com>
2021-03-12 16:18:29 -0700

    Removed extra padding from characers page.

--- src/tabs/Characters.jsx
+++ src/tabs/Characters.jsx
@@ -115,7 +115,7 @@ function Characters(props) {
   
 
   return(
-    <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4 overflow-hidden p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4 overflow-hidden">
       {list}
     </div>
   )
