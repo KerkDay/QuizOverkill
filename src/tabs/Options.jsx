0735072b29c6df2565e7597e27cfc51068da8d70
KerkDay <kerkday@outlook.com>
2021-03-12 16:19:15 -0700

    Fixed debounce issues, added class to a classless place

--- src/tabs/Options.jsx
+++ src/tabs/Options.jsx
@@ -3,36 +3,28 @@ import React, { useCallback, useState } from "react";
 function Options(props) {
   const [temp, setTemp] = useState(props.options)
 
+  // Update the temp options, then the global options
   const update = (val) => {
-    setTemp({
-      ...temp,
-      ...val
-    })
-    debounceUpdate(val)
+    setTemp({ ...temp, ...val })
+    debounce(debounceUpdate(val), 250)
   }
 
-  const debounceUpdate = useCallback(
-    debounce( val => {
-        props.setOptions({
-          ...temp,
-          ...val
-        })
-      }
-    )
-  )
+  // Update the global options
+  const debounceUpdate = val => props.setOptions({ ...temp, ...val })
+
+  const inputClass = "py-2 px-4 border border-gray-400 rounded-md"
 
   return(
-    <div className="mx-auto">
-      <div className="p-4 bg-gray-100 rounded-lg">
-        <label>
-          <input value={temp.title} onChange={event => {
-            update({title: [event.target.value]})
-          }} />
-        </label>
-      </div>
+    <div className="mx-auto p-4 bg-gray-100 rounded-lg w-full sm:w-1/2">
+      <label>
+        Title<br />
+        <input value={temp.title} onChange={event => {
+          update({title: event.target.value})
+        }}
+        className={`${inputClass} `}/>
+      </label>
     </div>
   )
-
 }
 
 export default Options;
