8ac1a75796078225f2c680afb5ed47092ec1d72d
KerkDay <kerkday@outlook.com>
2021-03-12 20:07:23 -0700

    Added link icon

--- src/extra/Icons.jsx
+++ src/extra/Icons.jsx
@@ -89,6 +89,14 @@ const Icons = {
         <line x1="3" y1="18" x2="21" y2="18"></line>
       </SVGBlock>
     )
+  },
+  Link: (props) => {
+    return(
+      <SVGBlock {...props}>
+        <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+        <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+      </SVGBlock>
+    )
   }
 }
 export default Icons
