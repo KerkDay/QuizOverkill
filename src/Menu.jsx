187fddf3b58ece1f76b3e1bcea24526b924bc8bb
KerkDay <kerkday@outlook.com>
2021-03-12 20:06:53 -0700

    More consice code, added icons to links

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -27,6 +27,8 @@ function Menu (props) {
 
       {/* The menu itself */}
       <div className={`${menuOpen} sm:block sm:max-h-screen h-screen transition-all absolute sm:static bg-gray-800 top-full left-0 right-0 z-50 sm:z-0 flex flex-col`}>
+
+        {/* Tabs */}
         <MenuTab {...props} name="options" onClick={()=> { changePage("options") }}>
           ⚙ Options
         </MenuTab>
@@ -39,9 +41,14 @@ function Menu (props) {
         <MenuTab {...props} name="testing" onClick={()=> { changePage("testing") }}>
           ✔ Testing
         </MenuTab>
+
+        {/* Links */}
         <div className={`p-2 justify-self-end`}>
           <MenuItem {...props}>
-            <a href="https://kerkday.com" target="_blank" className="no-underline">Created by Kerk Day</a>
+            <a href="https://kerkday.com" target="_blank" className="no-underline">
+              <Icons.Link className="h-4 inline-block mr-1"/>
+              Created by Kerk Day
+            </a>
           </MenuItem>
         </div>
       </div>
@@ -57,13 +64,13 @@ export default Menu
 
 // The tabs of the menu
 function MenuTab (props) {
-  let classes = "rounded-l-lg text-white p-2 my-2"
-  classes += props.name === props.page
+  // IF it's the current tab, then change the css a bit
+  let currentClass = props.name === props.page
   ? " bg-purple-500"
   : " ml-8 bg-purple-800 cursor-pointer"
 
   return (
-  <div className={classes} {...props}>
+  <div className={`${currentClass} rounded-l-lg text-white p-2 my-2`} {...props}>
     {props.children}
   </div>
   )
@@ -71,13 +78,13 @@ function MenuTab (props) {
 
 // The links in the menu
 function MenuItem (props) {
-  let classes = " text-white text-base text-right"
-  classes += props.name === props.page
-  ? " font-bold"
+  // If the link is an active page, make it bold!
+  let currentClass = props.name === props.page
+  ? "font-bold"
   : ""
 
   return (
-  <div className={classes}>
+  <div className={`${currentClass} text-gray-200 text-base text-right hover:text-gray-100`}>
     {props.children}
   </div>
   )
